0b44ea635fce8e3081bafb5c6ba6970ea8af4270
art2cat <yiming.whz@gmail.com>
2017-10-09 14:33:18 +0800

    update.

---
+++ JVMDemo/src/test/java/com/art2cat/dev/ch8/StaticDispatchTest.java
@@ -0,0 +1,35 @@
+package com.art2cat.dev.ch8;
+
+import org.junit.Test;
+
+public class StaticDispatchTest {
+	private StaticDispatch dispatch = new StaticDispatch();
+
+	@Test
+	public void test() {
+		StaticDispatch.Human man = new StaticDispatch.Man();
+		StaticDispatch.Human lady = new StaticDispatch.Woman();
+
+		dispatch.sayHello(man);
+		dispatch.sayHello(lady);
+	}
+
+	@Test
+	public void test1() {
+		StaticDispatch.Man man = new StaticDispatch.Man();
+		StaticDispatch.Woman lady = new StaticDispatch.Woman();
+
+		dispatch.sayHello(man);
+		dispatch.sayHello(lady);
+	}
+
+	@Test
+	public void test2() {
+		StaticDispatch.Human man = new StaticDispatch.Man();
+		man = new StaticDispatch.Woman();
+
+		if (man instanceof StaticDispatch.Man)
+			dispatch.sayHello((StaticDispatch.Man) man);
+		dispatch.sayHello((StaticDispatch.Woman) man);
+	}
+}
