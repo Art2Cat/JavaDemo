a74e79fbf6514c55eef70b772e5caac5698efc54
art2cat <yiming.whz@gmail.com>
2018-07-03 16:41:23 +0800

    add redisdemo

---
+++ redisdemo/src/main/java/com/art2cat/dev/dedisdemo/Application.java
@@ -0,0 +1,19 @@
+package com.art2cat.dev.dedisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+/**
+ * com.art2cat.dev.dedisdemo
+ *
+ * @author art2c
+ * @date 7/3/2018
+ */
+@SpringBootConfiguration
+public class Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+    
+}
