e5608bd018f7138800536c7b3e83bf41caf59625
rorschach.h <yiming.whz@gmail.com>
2019-02-25 16:50:50 +0800

    add CriteriaBuilder example.

--- jpademo/src/main/java/com/art2cat/dev/jpademo/services/controller/UserServiceController.java
+++ jpademo/src/main/java/com/art2cat/dev/jpademo/services/controller/UserServiceController.java
@@ -3,6 +3,13 @@ package com.art2cat.dev.jpademo.services.controller;
 import com.art2cat.dev.jpademo.UserRequest;
 import com.art2cat.dev.jpademo.models.User;
 import com.art2cat.dev.jpademo.services.intf.IUserService;
+import java.util.Objects;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
@@ -23,18 +30,22 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/user")
 public class UserServiceController {
-    
+
     @Autowired
     private IUserService userService;
-    
+
+    @Autowired
+    private EntityManager entityManager;
+
     @RequestMapping(value = "/getUser", method = RequestMethod.GET)
     public User getUser(@RequestParam(value = "id") Integer id) {
         return userService.getUser(id);
     }
-    
+
     @RequestMapping(value = "/insertUser", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<?> insertUser(@Valid @RequestBody UserRequest userRequest, Errors errors) {
+    public ResponseEntity<?> insertUser(@Valid @RequestBody UserRequest userRequest,
+        Errors errors) {
         String result;
         if (errors.hasErrors()) {
             // get all errors
@@ -42,18 +53,18 @@ public class UserServiceController {
                 .stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining(","));
-            
+
             return ResponseEntity.badRequest().body(result);
         }
         int returnCode = userService.insertUser(composeUser(userRequest));
         return ResponseEntity.ok(returnCode);
     }
-    
+
     @RequestMapping(value = "/deleteUser", method = RequestMethod.DELETE)
     public int deleteUser(@RequestParam(value = "id") Integer id) {
         return userService.deleteUser(id);
     }
-    
+
     @RequestMapping(value = "/updateUser", method = RequestMethod.PUT)
     public ResponseEntity<?> updateUser(@Valid @RequestBody UserRequest user, Errors errors) {
         String result;
@@ -63,13 +74,13 @@ public class UserServiceController {
                 .stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining(","));
-            
+
             return ResponseEntity.badRequest().body(result);
         }
         int returnCode = userService.updateUser(composeUser(user));
         return ResponseEntity.ok(returnCode);
     }
-    
+
     @RequestMapping(value = "/findUsers", method = RequestMethod.GET)
     public List<User> findUsers(@RequestParam(value = "userName") String userName,
         @RequestParam(value = "start") int start, @RequestParam(value = "limit") int limit) {
@@ -85,5 +96,16 @@ public class UserServiceController {
         user.setMobile(userRequest.getMobile());
         user.setNote(userRequest.getNote());
         return user;
+    }
+
+    @RequestMapping(value = "/getUserNameById", method = RequestMethod.GET)
+    public String getUserNameById(@RequestParam(value = "id") Integer id) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<String> userCriteriaQuery = builder.createQuery(String.class);
+        Root<User> root = userCriteriaQuery.from(User.class);
+        userCriteriaQuery.select(root.get("userName")).where(builder.equal(root.get("id"), id));
+        TypedQuery<String> query = entityManager.createQuery(userCriteriaQuery);
+        String userName = query.getSingleResult();
+        return userName;
     }
 }
