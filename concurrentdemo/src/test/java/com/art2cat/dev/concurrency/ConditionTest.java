e8cc64ac1fd995f0ebfe7ec5fd4c1cb11cccc029
rorschach.h <yiming.whz@gmail.com>
2019-03-22 10:37:27 +0800

    update.

--- concurrentdemo/src/test/java/com/art2cat/dev/concurrency/ConditionTest.java
+++ concurrentdemo/src/test/java/com/art2cat/dev/concurrency/ConditionTest.java
@@ -1,54 +1,59 @@
 package com.art2cat.dev.concurrency;
 
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.function.Predicate;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ConditionTest {
 
-    static Lock lock = new ReentrantLock();
-    static int index = 0;
-    static Map<Integer, Condition> cvs = new ConcurrentHashMap<>();
-
     @Test
     public void test() {
 
+        final String[] strings = new String[]{"A", "B", "C"};
+        final var lock = new ReentrantLock();
+        final var total = strings.length;
+        var ref = new Object() {
+            int token = 0;
+        };
+        final var cvs = new ConcurrentHashMap<Integer, Condition>(3);
         cvs.put(0, lock.newCondition());
         cvs.put(1, lock.newCondition());
         cvs.put(2, lock.newCondition());
-        Thread a = newPrinter(i -> i % 3 == 0, "A");
-        Thread b = newPrinter(i -> i % 3 == 1, "B");
-        Thread c = newPrinter(i -> i % 3 == 2, "C");
-        a.start();
-        b.start();
-        c.start();
-    }
 
-    private static Thread newPrinter(Predicate<Integer> predicate, String name) {
-        return new Thread(() -> {
-            while (index < 30) {
+        ThreadPoolExecutor pool = new TraceThreadPoolExecutor(3, 3, 0L, TimeUnit.MILLISECONDS,
+            new SynchronousQueue<>());
 
+        for (int i = 0; i < 3; i++) {
+            int finalI = i;
+            pool.submit(() -> {
+                lock.lock();
                 try {
-                    lock.lock();
-                    if (predicate.test(index)) {
-//                        var na = Thread.currentThread().getName();
-//                        var c = cvs.get(index % 3);
-//                        c.await();
-                        System.out.println(Thread.currentThread().getName());
-                        index++;
-//                        c.signalAll();
+                    for (int j = 0; j < 10; j++) {
+                        while (ref.token != finalI) {
+                            cvs.get(finalI).await();
+                        }
+                        System.out.println(strings[finalI]);
+                        ref.token = (ref.token + 1) % total;
+                        cvs.get(ref.token).signal();
                     }
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    Assertions.fail(e);
+                    e.printStackTrace();
                 } finally {
                     lock.unlock();
                 }
-            }
+            });
+        }
 
-        }, name);
+        try {
+            pool.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            Assertions.fail(e);
+        }
     }
 }
